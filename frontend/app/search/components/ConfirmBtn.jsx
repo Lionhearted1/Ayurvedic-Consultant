e941e83a0386c1f65762f17146dd115d7c10319c
Aditya Shetty <a4adityashetty@gmail.com>
2023-10-29 19:24:55 +0530

    Created confirm button

--- frontend/app/search/components/ConfirmBtn.jsx
+++ frontend/app/search/components/ConfirmBtn.jsx
@@ -5,13 +5,18 @@ const ConfirmBtn = (props) => {
   return (
     <>
       <motion.div
-        className={`confirm h-1/4 ${props.condition}`}
+        className={`confirm h-1/4 ${props.condition} flex justify-center items-center`}
         initial={{ opacity: 0, y: 10 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: 10 }}
         transition={{ duration: 0.5 }}
       >
-        Confirm
+        <button
+          className=" text-white bg-green-800 bg-opacity-75 w-[auto] h-[auto]
+         rounded-lg text-[1.1rem]  hover:bg-white hover:text-black hover:font-semibold px-[1rem] py-[0.5rem] lg:text-[1.2rem]"
+        >
+          Confirm
+        </button>
       </motion.div>
     </>
   );
