d4514e0d2a80309df6722eccf6bc7b65c5e4a223
Aditya Shetty <a4adityashetty@gmail.com>
2023-10-29 19:22:53 +0530

    Added Auto typing Component

---
+++ frontend/app/search/components/AutoTypingMessage.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { motion } from "framer-motion"; 
+
+const AutoTypingMessage = ({ condition,message }) => {
+  const [typedMessage, setTypedMessage] = useState('');
+
+  useEffect(() => {
+    let currentCharacter = 0;
+    const messageLength = message.length;
+
+    const typingInterval = setInterval(() => {
+      if (currentCharacter < messageLength) {
+        setTypedMessage(message.slice(0, currentCharacter + 1));
+        currentCharacter++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, 100); 
+
+    return () => {
+      clearInterval(typingInterval);
+    };
+  }, [message]);
+
+  return (
+    <div className={`text-white text-[2.5rem] md:text-[3rem] font-semibold ${condition}`}>
+      {typedMessage}
+    </div>
+  );
+};
+
+export default AutoTypingMessage;
