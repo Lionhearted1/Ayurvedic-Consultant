ab1f36fcc82c2445cd199ee9313cfe7ca764071b
Aditya Shetty <a4adityashetty@gmail.com>
2023-10-29 19:25:11 +0530

    Completed front end part of autocomplete

---
+++ frontend/app/search/components/SearchResult.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SearchResult = () => {
+  return (
+    <>
+     
+        <div className="w-full h-[2.5rem] p-4 bg-opacity-60 flex items-center hover:bg-white hover:bg-opacity-25 hover:shadow-lg hover:shadow-white/10">
+          Hello
+        </div>
+          <div className="w-full bg-white h-[0.05rem] bg-opacity-20"></div>
+      
+    </>
+  );
+};
+
+export default SearchResult;
