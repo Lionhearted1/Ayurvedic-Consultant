9db08b6db20c4e7f75918d82d42b605653ea15c0
varrun-v <varunsvmg2@gmail.com>
2023-10-30 15:33:47 +0530

    merge check

--- frontend/app/search/components/AutoTypingMessage.jsx
+++ frontend/app/search/components/AutoTypingMessage.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import React, { useEffect, useState } from 'react';
+import { motion } from "framer-motion"; 
 
-const AutoTypingMessage = ({ condition, message }) => {
-  const [typedMessage, setTypedMessage] = useState("");
+const AutoTypingMessage = ({ condition,message }) => {
+  const [typedMessage, setTypedMessage] = useState('');
 
   useEffect(() => {
     let currentCharacter = 0;
@@ -15,22 +15,18 @@ const AutoTypingMessage = ({ condition, message }) => {
       } else {
         clearInterval(typingInterval);
       }
-    }, 100);
+    }, 100); 
 
     return () => {
       clearInterval(typingInterval);
     };
   }, [message]);
 
-<<<<<<< HEAD
-  return <div className={`${condition}`}>{typedMessage}</div>;
-=======
   return (
-    <div className={`${condition}`}>
+    <div className={`text-white text-[2.5rem] md:text-[3rem] font-semibold ${condition}`}>
       {typedMessage}
     </div>
   );
->>>>>>> 857d5b906450c945348dceb576370665dcb1d6a4
 };
 
 export default AutoTypingMessage;
