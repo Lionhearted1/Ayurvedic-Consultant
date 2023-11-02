a23057d582d10a47eda8163e4456bec18fa48f64
varrun-v <varunsvmg2@gmail.com>
2023-11-02 17:46:29 +0530

    colour of navbar

--- frontend/app/components/DropdownMenu.jsx
+++ frontend/app/components/DropdownMenu.jsx
@@ -25,14 +25,14 @@ export default function Example() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg bg-opacity-25 ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
                 <a
                   href="#"
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    active ? 'bg-gray-100 text-gray-900' : 'text-white',
                     'block px-4 py-2 text-sm'
                   )}
                 >
@@ -45,7 +45,7 @@ export default function Example() {
                 <a
                   href="#"
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    active ? 'bg-gray-100 text-gray-900' : 'text-white',
                     'block px-4 py-2 text-sm'
                   )}
                 >
@@ -58,7 +58,7 @@ export default function Example() {
                 <a
                   href="#"
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    active ? 'bg-gray-100 text-gray-900' : 'text-white',
                     'block px-4 py-2 text-sm'
                   )}
                 >
@@ -72,7 +72,7 @@ export default function Example() {
                   <button
                     type="submit"
                     className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      active ? 'bg-gray-100 text-gray-900' : 'text-white',
                       'block w-full px-4 py-2 text-left text-sm'
                     )}
                   >
